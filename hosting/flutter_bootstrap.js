a2f8ad8a2af6e7451edce237e1008f462f2fff12
Mohamed Sherif <95943033+mosherif21@users.noreply.github.com>
2025-07-01 05:28:33 +0300

    update

--- hosting/flutter_bootstrap.js
+++ hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "221952424"
+    serviceWorkerVersion: "2243219241"
   }
 });
